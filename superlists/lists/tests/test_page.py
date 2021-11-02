e6817889421b48006b67228774abc6d5942ba597
priest4612 <prie4612@gmail.com>
2021-11-03 00:46:14 +0300

    Обновил код приложения lists

---
+++ superlists/lists/tests/test_page.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+    '''Тест домашней страницы'''
+
+    def test_root_url_resolves_to_home_page_view(self):
+        '''
+            тест: корневой url преобразуется в представление домашней страницы
+        '''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
--- superlists/lists/tests/test_smoke.py
+++ superlists/lists/tests/test_smoke.py
@@ -4,6 +4,6 @@ from django.test import TestCase
 class SmokeTest(TestCase):
     '''Тест на токсичность'''
 
-    def test_had_maths(self):
+    def test_bad_maths(self):
         '''тест: неправильные математические расчеты'''
         self.assertEqual(1 + 1, 3)
