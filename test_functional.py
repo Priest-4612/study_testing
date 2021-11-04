1028dcaedf7fc8f19dbb2138074a3879fe9aaa7b
priest4612 <prie4612@gmail.com>
2021-11-05 00:59:16 +0300

    Обновил тесты и views

--- test_functional.py
+++ test_functional.py
@@ -1,7 +1,9 @@
+import time
 import unittest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,19 +20,30 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_later(self):
         '''тест: можно начать список и получить его позже'''
-        # Эдит слышала про крутое новое онлайн-приложение со
-        # списком неотложных дел. Она решает оценить его
-        # домашнюю страницу
         self.service.start()
         self.browser = webdriver.Remote(self.service.service_url)
         self.browser.get('http://localhost:8000')
-
-        # Она видит, что заголовок и шапка страницы говорят о
-        # списках неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
 
-        # Ей сразу же предлагается ввести элемент списка
+        header_test = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_test)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.Enter)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(rows.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
+        self.fail('Закончить тест!')
 
 
 if __name__ == '__main__':
