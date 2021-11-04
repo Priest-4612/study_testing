8321cd6da9da2283ff0c0d7e1a612b5e74d92e75
priest4612 <prie4612@gmail.com>
2021-11-05 01:01:03 +0300

    Обновил тесты

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -1,24 +1,10 @@
 from django.test import TestCase
-from django.urls import resolve
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
     '''Тест домашней страницы'''
 
-    def test_root_url_resolves_to_home_page_view(self):
-        '''
-            тест: корневой url преобразуется в представление домашней страницы
-        '''
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        '''тест: домашняя страница возвращает правильный html'''
+    def test_uses_home_template(self):
+        '''тест: используется правильный шаблон'''
         response = self.client.get('/')
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<!DOCTYPE html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
         self.assertTemplateUsed(response, 'home.html')
