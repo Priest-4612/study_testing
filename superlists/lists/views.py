f6881bb27d81dbffe04d15ed659565853a5080ec
priest4612 <prie4612@gmail.com>
2021-11-05 02:17:00 +0300

    Обновил код

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def home_page(request):
     '''Домашняя страница'''
-    return render(request, 'home.html')
+    context = {'new_item_text': request.POST.get('item_text', '')}
+    return render(request, 'home.html', context=context)
