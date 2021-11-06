c68ca6c7c074e9fce8383ebbe7d3fa8dce292640
priest4612 <prie4612@gmail.com>
2021-11-07 00:33:08 +0300

    Убрал комментирование с теста

--- superlists/tests_functional/test_functional.py
+++ superlists/tests_functional/test_functional.py
@@ -82,3 +82,17 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Купить павлиньи перья', page_text)
         self.assertIn('Купить молоко')
+
+    def test_layout_and_styling(self):
+        '''тест макета и стилевого оформления'''
+        self.service.start()
+        self.browser = webdriver.Remote(self.service.service_url)
+        self.browser.get(self.live_server_url)
+
+        self.browser.set_window_size(1024, 768)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
